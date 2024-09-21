b00829760021720780b6fbb51c70d763ae61ab11
eld13g0 <82134232+eld13g0@users.noreply.github.com>
2024-09-21 16:30:11 -0300

    Cambio QR

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -20,7 +20,7 @@ import { ApiClientService } from 'src/app/services/api-client.service';
 })
 export class InicioPage implements OnInit {
 
-  componente_actual = 'misdatos';
+  componente_actual = 'qr';
 
   constructor(
     private authService: AuthService, 
@@ -29,7 +29,7 @@ export class InicioPage implements OnInit {
 
   ngOnInit() {
     this.authService.primerInicioSesion.subscribe(esPrimerInicioSesion => {
-      this.componente_actual = 'misdatos';
+      this.componente_actual = 'qr';
       this.bd.datosQR.next('');
     });
   }
